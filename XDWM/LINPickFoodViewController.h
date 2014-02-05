f37008031542d715d5381df2b4133a0a6b5eb41d
fjswhl <lin@lin.local>
2014-02-05 11:35:00 +0800

    b

---
+++ XDWM/LINPickFoodViewController.h
@@ -0,0 +1,15 @@
+//
+//  LINPickFoodViewController.h
+//  XDWM
+//
+//  Created by Lin on 14-2-5.
+//  Copyright (c) 2014年 WeAround. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LINPickFoodViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *foodList;
+@property (nonatomic) NSInteger kFoodKindIndex;
+@end
