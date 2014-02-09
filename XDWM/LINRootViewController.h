063eee4a4ebf590d32c92ebe7d8a6dcd19abedcd
fjswhl <lin@lin.local>
2014-02-09 17:04:43 +0800

    master

---
+++ XDWM/LINRootViewController.h
@@ -0,0 +1,14 @@
+//
+//  LINRootViewController.h
+//  XDWM
+//
+//  Created by Lin on 14-2-9.
+//  Copyright (c) 2014年 WeAround. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LINUserModel.h"
+@interface LINRootViewController : UITableViewController
+
+@property (nonatomic, strong)   LINUserModel *user;
+@end
